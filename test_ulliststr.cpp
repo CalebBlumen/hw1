2d19d25732265a0a0f24e03c14752d664c8d935c
Caleb Blumenfeld <cblumenf@usc.edu>
2024-01-30 00:54:00 +0000

    HW1 part 2 finished

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,52 @@
 #include <sstream>
 
 #include "ulliststr.h"
+using namespace std;
 
 //Use this file to test your ulliststr implementation before running the test suite
+void test();
 
 int main(int argc, char* argv[])
 {
+ 
+  test();
+  return 0;
+}
+
+void test() {
+  ULListStr testList;
+
+  testList.push_back("a");
+  testList.get(0);
+  testList.pop_front();
+  cout << testList.size() << endl;
+  
+ /* cout << "populating: " << endl;
+  for (unsigned int i = 0; i < 11; ++i) {
+    testList.push_back(to_string(i));
+  }
+  for (unsigned int i = 0; i < 12; ++i) {
+    testList.push_front(to_string(i));
+  }
+  
+  cout << endl;
+  cout << "actual size: " << testList.size() << " list size: " << testList.size() << endl;
+  
+  cout << "full list: " << endl;
+  testList.printList();
+
+  cout << "back: " << testList.back() << "; front: " << testList.front() << endl;
+
+  cout << "Popping front and back: " << endl;
+  testList.pop_back();
+  testList.pop_front();
+
+  testList.printList();
+
+  cout << "Getting index 0: ";
+  cout << testList.get(0) << endl;
+
+  cout << "back: " << testList.back() << "; front: " << testList.front() << endl;
+*/
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,120 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val) {
+  if (empty()) {
+    Item* newItem = new Item;
+    newItem->val[0] = val;
+    newItem->last = 1;
+    head_ = newItem;
+    tail_ = newItem;
+    
+  }
+  else if (ARRSIZE - tail_->last) {
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+  }
+  else {
+    Item* newItem = new Item;
+    newItem->val[0] = val;
+    newItem->last = 1;
+    tail_->next = newItem;
+    newItem->prev = tail_;
+    tail_ = newItem;
+  }
+  size_++;
+}
+void ULListStr::push_front(const std::string& val) {
+  if (empty()) {
+    Item* newItem = new Item;
+    newItem->val[0] = val;
+    newItem->last = 1;
+    head_ = newItem;
+    tail_ = newItem;
+  }
+  else if (head_->first) {
+    head_->val[head_->first-1] = val;
+    head_->first--;
+  }
+  else {
+    Item* newItem = new Item;
+    newItem->val[ARRSIZE-1] = val;
+    newItem->first = ARRSIZE-1;
+    newItem->last = 10;
+    head_->prev = newItem;
+    newItem->next = head_;
+    head_ = newItem;
+    head_->prev = nullptr;
+  }
+  size_++;
+}
+void ULListStr::pop_back() {
+  if (empty()) {
+    return;
+  }
+  else if (size_ == 1) {
+    clear();
+    return;
+  }
+  else if (tail_->last > 1) {
+    tail_->val[tail_->last-1] = "";
+    tail_->last--;
+  }
+  else {
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    delete temp;
+    tail_->next = nullptr;
+  }
+  size_--;
+}
+void ULListStr::pop_front() {
+  if (empty()) {
+    return;
+  }
+  else if (size_ == 1) {
+    clear();
+    return;
+  }
+  else if (head_->first < 9) {
+    head_->val[head_->first] = "";
+    head_->first++;
+  }
+  else {
+    Item* temp = head_;
+    head_ = head_->next;
+    delete temp;
+  }
+  size_--;
+}
+std::string const & ULListStr::back() const {
+  return tail_->val[tail_->last-1];
+}
+std::string const & ULListStr::front() const {
+  return head_->val[head_->first];
+}
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  if (loc > size_-1 || loc < 0) {
+    return NULL;
+  }
+  Item* temp = head_;
+  int numNext;
+  int backfill;
+  int calcHelp = loc-ARRSIZE+head_->first;
+  if (calcHelp >= 0) {
+    numNext = calcHelp/ARRSIZE+1;
+    backfill = calcHelp % ARRSIZE;
+  } 
+  else {
+    numNext = 0;
+    backfill = head_->first + loc;
+  }
+  for (int i = 0; i < numNext; ++i) {
+    temp = temp->next;
+  }
+  std::string* ret = &temp->val[backfill];
+  return ret;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -63,3 +177,5 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+
